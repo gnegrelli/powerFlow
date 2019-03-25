c9f8b94832c942509d4bbb3c754c03d1f450edf7
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-03-25 14:00:46 -0300

    Create bus objects

--- PowerFlow.py
+++ PowerFlow.py
@@ -3,14 +3,36 @@ import numpy as np
 
 # Class of system buses
 class Bus:
-    def __init__(self, ID, name, bustype, V, theta, P , Q):
-        self.ID = ID
-        self.name = name
-        self.bustype = bustype
-        self.V = V
-        self.theta = theta
-        self.P = P
-        self.Q = Q
+    def __init__(self, databus):
+
+        bustypes = {'0': 'PQ', '1': 'PV', '2': 'Vθ'}
+
+        self.ID = int(databus[0:4])
+        self.name = databus[8:22]
+        self.bustype = bustypes[databus[4:8].strip()]
+
+        self.V = float(databus[22:26])/1000
+        if databus[26:30].strip():
+            self.theta = float(databus[26:30])
+        else:
+            self.theta = 0
+
+        P = []
+        Q = []
+        for item in [databus[30:35], databus[56:60]]:
+            if not item.strip():
+                P.append(0)
+            else:
+                P.append(float(item))
+
+        for item in [databus[35:40], databus[60:65]]:
+            if not item.strip():
+                Q.append(0)
+            else:
+                Q.append(float(item))
+
+        self.P = P[0] - P[1]
+        self.Q = Q[0] - Q[1]
 
 
 # Class of system lines
@@ -22,3 +44,14 @@ class Line:
         self.X = X
         self.B = B
 
+
+rawData = open("Monticelli_ex5_2.txt", "r").read()
+datasets = rawData.split("9999\n")
+
+# Create bus objects
+buses = dict()
+bus_set = datasets[0].split('\n')
+
+for row in bus_set:
+    if row.strip():
+        buses[str(int(row[0:4]))] = Bus(row)
